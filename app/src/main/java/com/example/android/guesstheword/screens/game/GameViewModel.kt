dcdd85eb425c5d6a6b0284600dfe89813c4845fa
Lavergne <hugo.lavergne@devinci.fr>
2019-12-26 13:36:14 +0100

    refactoring

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,17 +1,96 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
 
-class GameViewModel : ViewModel(){
+class GameViewModel : ViewModel() {
 
-    private lateinit var viewModel: GameViewModel
-    init {
-        Log.i("GameViewModel", "GameViewModel created !")
+    // The current word
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
     }
 
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
+        resetList()
+        nextWord()
+    }
+
+    /**
+     * Callback called when the ViewModel is destroyed
+     */
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel" , "GameViewModel Destroyed")
+        Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+
+    /** Methods for updating the UI **/
+    fun onSkip() {
+        if (!wordList.isEmpty()) {
+            score--
+        }
+        nextWord()
+    }
+    fun onCorrect() {
+        if (!wordList.isEmpty()) {
+            score++
+        }
+        nextWord()
+    }
+
+    /**
+     * Moves to the next word in the list.
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (!wordList.isEmpty()) {
+            word = wordList.removeAt(0)
+        }
     }
 }
